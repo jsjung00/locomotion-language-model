e12624269c63afc0cbea1a10a23015887ae34a95
Justin Jung <justinsoljung@gmail.com>
2025-05-31 22:19:46 +0000

    fix batch size, get dt working

--- decision_transformer/training/trainer.py
+++ decision_transformer/training/trainer.py
@@ -1,12 +1,12 @@
 import numpy as np
 import torch
+from tqdm import tqdm 
 
 import time
 
 
 class Trainer:
-
-    def __init__(self, model, optimizer, batch_size, get_batch, loss_fn, scheduler=None, eval_fns=None):
+    def __init__(self, model, optimizer, batch_size, get_batch, loss_fn, scheduler=None, eval_fns=None, eval_every_n_epochs=100):
         self.model = model
         self.optimizer = optimizer
         self.batch_size = batch_size
@@ -15,18 +15,18 @@ class Trainer:
         self.scheduler = scheduler
         self.eval_fns = [] if eval_fns is None else eval_fns
         self.diagnostics = dict()
+        self.eval_every_n_epochs = eval_every_n_epochs
 
         self.start_time = time.time()
 
     def train_iteration(self, num_steps, iter_num=0, print_logs=False):
-
         train_losses = []
         logs = dict()
 
         train_start = time.time()
 
         self.model.train()
-        for _ in range(num_steps):
+        for _ in tqdm(range(num_steps)):
             train_loss = self.train_step()
             train_losses.append(train_loss)
             if self.scheduler is not None:
@@ -34,16 +34,17 @@ class Trainer:
 
         logs['time/training'] = time.time() - train_start
 
-        eval_start = time.time()
+        if (iter_num+1) % self.eval_every_n_epochs == 0:
+            eval_start = time.time()
 
-        self.model.eval()
-        for eval_fn in self.eval_fns:
-            outputs = eval_fn(self.model)
-            for k, v in outputs.items():
-                logs[f'evaluation/{k}'] = v
+            self.model.eval()
+            for eval_fn in self.eval_fns:
+                outputs = eval_fn(self.model)
+                for k, v in outputs.items():
+                    logs[f'evaluation/{k}'] = v
+            logs['time/evaluation'] = time.time() - eval_start
 
         logs['time/total'] = time.time() - self.start_time
-        logs['time/evaluation'] = time.time() - eval_start
         logs['training/train_loss_mean'] = np.mean(train_losses)
         logs['training/train_loss_std'] = np.std(train_losses)
 
--- experiment.py
+++ experiment.py
@@ -218,16 +218,7 @@ def experiment(
             scheduler=scheduler,
             loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a)**2),
             eval_fns=[eval_episodes(tar) for tar in env_targets],
-        )
-    elif model_type == 'bc':
-        trainer = ActTrainer(
-            model=model,
-            optimizer=optimizer,
-            batch_size=batch_size,
-            get_batch=get_batch,
-            scheduler=scheduler,
-            loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a)**2),
-            eval_fns=[eval_episodes(tar) for tar in env_targets],
+            eval_every_n_epochs=5
         )
 
     if log_to_wandb:
@@ -250,23 +241,23 @@ if __name__ == '__main__':
     parser.add_argument('--env', type=str, default='halfcheetah')
     parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
     parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
-    parser.add_argument('--K', type=int, default=20)
+    parser.add_argument('--K', type=int, default=128)
     parser.add_argument('--pct_traj', type=float, default=1.)
-    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--batch_size', type=int, default=1028)
     parser.add_argument('--model_type', type=str, default='dt')  # dt for decision transformer, bc for behavior cloning
     parser.add_argument('--embed_dim', type=int, default=128)
     parser.add_argument('--n_layer', type=int, default=3)
     parser.add_argument('--n_head', type=int, default=1)
     parser.add_argument('--activation_function', type=str, default='relu')
     parser.add_argument('--dropout', type=float, default=0.1)
-    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
+    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-3)
     parser.add_argument('--weight_decay', '-wd', type=float, default=1e-4)
     parser.add_argument('--warmup_steps', type=int, default=10000)
     parser.add_argument('--num_eval_episodes', type=int, default=100)
     parser.add_argument('--max_iters', type=int, default=10)
-    parser.add_argument('--num_steps_per_iter', type=int, default=10000)
+    parser.add_argument('--num_steps_per_iter', type=int, default=100)
     parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--log_to_wandb', '-w', type=bool, default=False)
+    parser.add_argument('--log_to_wandb', '-w', type=bool, default=True)
     
     args = parser.parse_args()
 
